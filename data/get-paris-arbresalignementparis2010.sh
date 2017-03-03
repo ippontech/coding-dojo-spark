57fd0bdd5dd94f9bc3a87d65f3cd772e1df6bc5f
Antoine HARS <ahars@ippon.fr>
2017-03-03 15:16:51 +0100

    delete scripts to download data

--- data/get-paris-arbresalignementparis2010.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -e
-
-mkdir paris-arbresalignementparis2010 && cd $_
-
-curl http://opendata.paris.fr/explore/dataset/arbresalignementparis2010/download/?format=csv > arbresalignementparis2010.csv
-tail -n +2 arbresalignementparis2010.csv > arbresalignementparis2010_noheader.csv
