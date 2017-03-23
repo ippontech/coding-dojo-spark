be60d21e7a8e89235afa4ef516291c46c7300b1e
Thomas Cozien <thomascozien@MacBook-Pro-de-Thomas.local>
2017-03-23 16:37:21 +0100

    clean code

--- dojo-spark/src/main/scala/fr/ippon/dojo/spark/FirstNameDataFrame.scala
+++ dojo-spark/src/main/scala/fr/ippon/dojo/spark/FirstNameDataFrame.scala
@@ -32,23 +32,14 @@ object FirstNameDataFrame {
     //
     // Nombre de prénoms différents
     //
-    val nbFirstName = df.where(!df("preusuel").startsWith("_"))
-      .select(df("preusuel"))
-      .distinct()
-
+    val nbFirstName = ???
 
     // System.out.println("Nb FirstName : " + nbFirstName.count())
 
     //
     // Top 10 des prénoms de l'année 2010
     //
-    val top10FirstName = df.where(!df("preusuel").startsWith("_") && df("annais") === "2010")
-      .groupBy(df("preusuel"))
-      .agg(functions.sum(df("nombre")).as("sum"))
-
-    top10FirstName.sort(top10FirstName("sum").desc)
-      .show(10)
-
+    val top10FirstName = ???
 
     spark.stop()
   }
--- dojo-spark/src/main/scala/fr/ippon/dojo/spark/FirstNameRdd.scala
+++ dojo-spark/src/main/scala/fr/ippon/dojo/spark/FirstNameRdd.scala
@@ -46,27 +46,14 @@ object FirstNameRdd {
     //
     // Nombre de prénoms différents
     //
-    val nbFirstName = rdd.filter(s => !s.startsWith("sexe"))
-      .map(line => line.split(SEPARATOR))
-      .filter(x => !x(1).startsWith("_"))
-      .map(x => x(1))
-      .distinct()
-
+    val nbFirstName = ???
 
     // System.out.println("Nb FirstName : " + nbFirstName.count())
 
     //
     // Top 10 prénoms de l'année 2010
     //
-    val top10FirstName = rdd.filter(s => !s.startsWith("sexe"))
-      .map(line => line.split(SEPARATOR))
-      .filter(x => x(2) == "2010" && !x(1).startsWith("_"))
-      .map(x => (x(1), x(4).toFloat))
-      .reduceByKey((x, y) => x + y)
-      .sortBy(_._2, ascending = false)
-      .take(10)
-      .foreach(println)
-
+    val top10FirstName = ???
 
     sc.stop()
   }
