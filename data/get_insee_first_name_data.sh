7aa8b0dc9b5cbfea057e05faad2d0f9663efe748
Antoine HARS <ahars@ippon.fr>
2017-03-27 11:26:13 +0200

    add check on the number of lines for first_name data

--- data/get_insee_first_name_data.sh
+++ data/get_insee_first_name_data.sh
@@ -2,12 +2,21 @@
 
 dir=../dojo-spark/src/main/resources/data/insee
 
+count=3405312
+
 dptZip=insee_first_name_dpt.zip
+fileName=dpt2015.txt
+url="https://www.insee.fr/fr/statistiques/fichier/2540004/dpt2015_txt.zip"
 
 mkdir -p $dir
 cd $dir
 
-curl https://www.insee.fr/fr/statistiques/fichier/2540004/dpt2015_txt.zip > $dptZip
+curl $url > $dptZip
 unzip -o $dptZip
-
 rm $dptZip
+
+if [ $(wc -l $fileName | awk '{print $1}') -eq $count ]; then
+        echo file correctly downloaded
+else
+        echo download failed..
+fi
