64b4b7992ae439a4e13d5f0433dc10caf3013d06
Antoine HARS <ahars@ippon.fr>
2017-03-27 11:49:18 +0200

    change correction of the exercice 1

--- dojo-spark/src/main/scala/fr/ippon/dojo/spark/Exercice1.scala
+++ dojo-spark/src/main/scala/fr/ippon/dojo/spark/Exercice1.scala
@@ -3,55 +3,85 @@ package fr.ippon.dojo.spark
 import org.apache.spark.{SparkConf, SparkContext}
 
 /**
-  * US#1 : Rdds
+  * Exercice 1
+  * Cas d'utilisation des RDDs sur le jeu de données des prénoms
   */
 object Exercice1 {
 
-  val PATH: String = "/home/dojo/workspace/coding-dojo-spark/"
-  val FILE_PATH: String = PATH + "/data/insee/dpt2015.txt"
-  val SEPARATOR: String = "\t";
+  case class Prenom(sexe: String, preusuel: String, annais: String, dpt: String, nombre: Double)
 
   def main(args: Array[String]) {
 
-    // Spark configuration
+    // configuration du job Spark
     val conf = new SparkConf()
-      .setAppName("Dojo Spark-Cassandra [FirstNameRdd]")
-      .setMaster("local[*]")
-    // Spark context
+      .setAppName("Coding Dojo Data - Exercice 1")
+      .setMaster("local")
+
+    // instanciation du Spark Context
     val sc = new SparkContext(conf)
 
-    // Load file
-    val rdd = sc.textFile(FILE_PATH)
+    // chargez le fichier dans un RDD
+    val initialFirstNameRdd = sc.textFile("src/main/resources/data/insee/dpt2015.txt")
 
-    // rdd.cache()
+    // faites un comptage du nombre de ligne et affichez le dans le terminal
+    println("comptage initial = " + initialFirstNameRdd.count)
 
-    //
-    // Nombre de prénoms différents
-    //
-    val nbFirstName = rdd.filter(s => !s.startsWith("sexe"))
-      .map(line => line.split(SEPARATOR))
-      .filter(x => !x(1).startsWith("_"))
-      .map(x => x(1))
-      .distinct()
+    // affichez les 20 premiers éléments
+    println("\nRDD initial")
+    initialFirstNameRdd
+      .take(20)
+      .foreach(println)
 
+    // créez une case class en dehors de la méthode main avec le nom des colonnes du header
 
-    // System.out.println("Nb FirstName : " + nbFirstName.count())
+    // créez un nouveau rdd en supprimant la première ligne
+    // puis découpez les autres par le délimiteur
+    // et appliquez votre tableau de string dans la case class que vous avez créé
+    val firstNameRdd = initialFirstNameRdd
+      .filter(s => !s.startsWith("sexe"))
+      .map(line => line.split("\t"))
+      .map(k => Prenom(k(0), k(1), k(2), k(3), k(4).toDouble))
 
-    //
-    // Top 10 prénoms de l'année 2010
-    //
-    val top10FirstName = rdd.filter(s => !s.startsWith("sexe"))
-      .map(line => line.split(SEPARATOR))
-      .filter(x => x(2) == "2010" && !x(1).startsWith("_"))
-      .map(x => (x(1), x(4).toFloat))
-      .reduceByKey((x, y) => x + y)
+    // mettez ce RDD en cache
+    val cachedFirstNameRdd = firstNameRdd.cache
+
+    // afficher les 20 premières lignes de ce nouveau RDD
+    println("\nRDD formaté :")
+    cachedFirstNameRdd
+      .take(20)
+      .foreach(println)
+
+    // trouvez le nombre de personne qui ont le même prénom que vous
+    println("\nNombre de personnes pour le nom donné : " + cachedFirstNameRdd
+      .filter(_.preusuel == "ANTOINE")
+      .map(_.nombre)
+      .sum)
+
+    // êtes-vous le seul à avoir eu votre prénom l'année de votre naissance dans votre département ?
+    println("\nNombre de personnes pour un nom, un département et une année donnés : " + cachedFirstNameRdd
+      .filter(_.preusuel == "ANTOINE")
+      .filter(_.dpt == "59")
+      .filter(_.annais == "1990")
+      .map(_.nombre)
+      .collect()(0))
+
+    // déterminez le nombre total de prénoms différents
+    println("\nNombre total de prénoms différents : " + cachedFirstNameRdd
+      .map(_.preusuel)
+      .distinct
+      .count)
+
+    // trouvez le top 10 des prénoms utilisés pour l'année de votre naissance
+    // vous pouvez créer un tuple en étape intermédiaire (String, Float)
+    println("\nTop 10 des prénoms utilisés pour une année donnée :")
+    cachedFirstNameRdd
+      .filter(_.annais == "1990")
+      .map(l => (l.preusuel, l.nombre.toFloat))
+      .reduceByKey(_ + _)
       .sortBy(_._2, ascending = false)
       .take(10)
       .foreach(println)
 
-
-    sc.stop()
+    sc.stop
   }
 }
-
-// scalastyle:on println
