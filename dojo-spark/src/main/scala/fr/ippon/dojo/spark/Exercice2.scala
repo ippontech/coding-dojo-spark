f91e00a4795fd43614780bead6e6398b2da72864
Antoine HARS <ahars@ippon.fr>
2017-03-27 13:51:44 +0200

    update exercice2

--- dojo-spark/src/main/scala/fr/ippon/dojo/spark/Exercice2.scala
+++ dojo-spark/src/main/scala/fr/ippon/dojo/spark/Exercice2.scala
@@ -1,55 +1,83 @@
 package fr.ippon.dojo.spark
 
-import org.apache.spark.sql.{SparkSession, functions}
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
 
 /**
-  * US#2 : DataFrames
+  * Exercice 2
+  * Cas d'utilisation des DataFrames / DataSets sur le jeu de données des prénoms
   */
 object Exercice2 {
 
-  val PATH: String = "/home/dojo/workspace/coding-dojo-spark/"
-  val FILE_PATH: String = PATH + "/data/insee/dpt2015.txt"
-
   def main(args: Array[String]) {
 
-    // Spark configuration
+    // instanciation du Spark Session
     val spark = SparkSession
       .builder
-      .appName("Dojo Spark-Cassandra [FirstNameDataFrame]")
-      .master("local[*]")
-      .getOrCreate()
+      .master("local")
+      .appName("Coding Dojo - Exercice 2")
+      .getOrCreate
 
-    // Load file
-    val df = spark.read
+    // chargez le fichier dans un DataFrame ou DataSet et mettez le en cache
+    val firstNameDf = spark
+      .read
       .option("header", true)
       .option("delimiter", "\t")
       .option("inferSchema", "true")
-      .csv(FILE_PATH)
+      .csv("src/main/resources/data/insee/dpt2015.txt")
+      .cache
 
-    // df.cache()
-    // df.show()
+    // faites un comptage du nombre de ligne et affichez le dans le terminal
+    println("nombre de lignes du fichier : " + firstNameDf.count)
 
-    //
-    // Nombre de prénoms différents
-    //
-    val nbFirstName = df.where(!df("preusuel").startsWith("_"))
-      .select(df("preusuel"))
-      .distinct()
+    // affichez le schéma du dataframe
+    println("\nSchéma des données :")
+    firstNameDf.printSchema
 
+    // affichez les 20 premiers éléments
+    println("\nExtrait de données :")
+    firstNameDf.show
 
-    // System.out.println("Nb FirstName : " + nbFirstName.count())
+    // trouvez le nombre de personne qui ont le même prénom que vous
+    println("\nNombre de personnes avec un même prénom donné :")
+    firstNameDf
+      .where(col("preusuel") === "ANTOINE")
+      .groupBy("preusuel")
+      .sum("nombre")
+      .show
 
-    //
-    // Top 10 des prénoms de l'année 2010
-    //
-    val top10FirstName = df.where(!df("preusuel").startsWith("_") && df("annais") === "2010")
-      .groupBy(df("preusuel"))
-      .agg(functions.sum(df("nombre")).as("sum"))
+    // êtes-vous le seul à avoir eu votre prénom l'année de votre naissance dans votre département ?
+    println("\nNombre de personnes pour un nom, un département et une année donnés :")
+    firstNameDf
+      .where(
+        col("preusuel") === "ANTOINE"
+          && col("dpt") === "59"
+          && col("annais") === "1990")
+      .show
 
-    top10FirstName.sort(top10FirstName("sum").desc)
-      .show(10)
+    // déterminez le nombre total de prénoms différents
+    println("\nNombre de prénoms différents : " + firstNameDf
+      .select("preusuel")
+      .distinct
+      .count)
 
+    // trouvez le top 10 des prénoms utilisés pour l'année de votre naissance
+    // vous pouvez caster le nombre en Float
+    val top10FirstNameDf = firstNameDf
+      .where(col("annais") === "1990")
+      .select(col("preusuel"), col("nombre"))
+      .groupBy("preusuel")
+      .agg(sum("nombre").as("sum"))
+      .orderBy(col("sum").desc)
+      .limit(10)
 
-    spark.stop()
+    println("\nTop10 des prénoms pour une année donnée :")
+    top10FirstNameDf.show
+
+    // affichez le plan d'exécution de ce dernier dataframe
+    println("\nPlan d'exécution du DataFrame :")
+    top10FirstNameDf.explain
+
+    spark.stop
   }
 }
