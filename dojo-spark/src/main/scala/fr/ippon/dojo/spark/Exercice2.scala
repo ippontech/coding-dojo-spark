4511c52840725a1fe5bdfb7fef00a066c0277947
Antoine HARS <ahars@ippon.fr>
2017-03-28 11:00:45 +0200

    add exercice 2 scala object

---
+++ dojo-spark/src/main/scala/fr/ippon/dojo/spark/Exercice2.scala
@@ -0,0 +1,50 @@
+package fr.ippon.dojo.spark
+
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
+
+/**
+  * Exercice 2
+  * Cas d'utilisation des DataFrames / DataSets sur le jeu de données des prénoms
+  */
+object Exercice2 {
+
+  def main(args: Array[String]) {
+
+    // instanciation du Spark Session
+    val spark = SparkSession
+      .builder
+      .master("local")
+      .appName("Coding Dojo - Exercice 2")
+      .getOrCreate
+
+    // chargez le fichier dans un DataFrame ou DataSet et mettez le en cache
+
+
+    // faites un comptage du nombre de ligne et affichez le dans le terminal
+
+
+    // affichez le schéma du dataframe
+
+
+    // affichez les 20 premiers éléments
+
+
+    // trouvez le nombre de personne qui ont le même prénom que vous
+
+
+    // êtes-vous le seul à avoir eu votre prénom l'année de votre naissance dans votre département ?
+
+
+    // déterminez le nombre total de prénoms différents
+
+
+    // trouvez le top 10 des prénoms utilisés pour l'année de votre naissance
+
+
+    // affichez le plan d'exécution de ce dernier dataframe
+
+
+    spark.stop
+  }
+}
