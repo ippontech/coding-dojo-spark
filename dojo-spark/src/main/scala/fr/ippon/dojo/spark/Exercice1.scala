1c4efd04814721ed23f373162670a9498ef215fc
wconti <wconti@ippon.fr>
2018-03-20 17:10:24 +0100

    Ajout de la branche d'exercice sur spark 2.3

--- dojo-spark/src/main/scala/fr/ippon/dojo/spark/Exercice1.scala
+++ dojo-spark/src/main/scala/fr/ippon/dojo/spark/Exercice1.scala
@@ -8,82 +8,67 @@ import org.apache.spark.{SparkConf, SparkContext}
   */
 object Exercice1 {
 
-  case class Prenom(sexe: String, preusuel: String, annais: String, dpt: String, nombre: Int)
-
   def main(args: Array[String]) {
 
-    // configuration du job Spark
-    val conf = new SparkConf()
-      .setAppName("Coding Dojo Data - Exercice 1")
-      .setMaster("local")
+    // instanciation du Spark Conf
 
+
+    //----------------------------------------------------------------------
     // instanciation du Spark Context
-    val sc = new SparkContext(conf)
 
+
+    //----------------------------------------------------------------------
     // chargez le fichier dans un RDD
-    val initialFirstNameRdd = sc.textFile("src/main/resources/data/insee/dpt2016.txt")
+    //val initialFirstNameRdd = ...
 
+
+    //----------------------------------------------------------------------
     // faites un comptage du nombre de ligne et affichez le dans le terminal
-    println("comptage initial = " + initialFirstNameRdd.count)
 
+
+    //----------------------------------------------------------------------
     // affichez les 20 premiers éléments
-    println("\nRDD initial")
-    initialFirstNameRdd
-      .take(20)
-      .foreach(println)
 
+
+    //----------------------------------------------------------------------
     // créez une case class en dehors de la méthode main avec le nom des colonnes du header
-
     // créez un nouveau rdd en supprimant la première ligne
     // puis découpez les autres par le délimiteur
     // et appliquez votre tableau de string dans la case class que vous avez créé
-    val firstNameRdd = initialFirstNameRdd
-      .filter(s => !s.startsWith("sexe"))
-      .map(line => (line.split("\t")))
-      .filter(_.length > 1)
-      .map(k => Prenom(k.head, k(1), k(2), k(3), k.last.toInt))
+    //val firstNameRdd = ...
 
+
+    //----------------------------------------------------------------------
     // mettez ce RDD en cache
-    val cachedFirstNameRdd = firstNameRdd.cache
+    //val cachedFirstNameRdd =
 
+
+    //----------------------------------------------------------------------
     // afficher les 20 premières lignes de ce nouveau RDD
-    println("\nRDD formaté :")
-    cachedFirstNameRdd
-      .take(20)
-      .foreach(println)
 
 
+    //----------------------------------------------------------------------
     // trouvez le nombre de personne qui ont le même prénom que vous
-    println("\nNombre de personnes pour le nom donné : " + cachedFirstNameRdd
-      .filter(_.preusuel == "ANTOINE")
-      .map(_.nombre)
-      .sum)
 
+
+    //----------------------------------------------------------------------
     // êtes-vous le seul à avoir eu votre prénom l'année de votre naissance dans votre département ?
-    println("\nNombre de personnes pour un nom, un département et une année donnés : " + cachedFirstNameRdd
-      .filter(_.preusuel == "ANTOINE")
-      .filter(_.dpt == "59")
-      .filter(_.annais == "1990")
-      .map(_.nombre)
-      .collect()(0))
 
+
+    //----------------------------------------------------------------------
     // déterminez le nombre total de prénoms différents
-    println("\nNombre total de prénoms différents : " + cachedFirstNameRdd
-      .map(_.preusuel)
-      .distinct
-      .count)
 
+
+    //----------------------------------------------------------------------
     // trouvez le top 10 des prénoms utilisés pour l'année de votre naissance
     // vous pouvez créer un tuple en étape intermédiaire (String, Float)
-    println("\nTop 10 des prénoms utilisés pour une année donnée :")
-    cachedFirstNameRdd
-      .filter(_.annais == "1990")
-      .map(l => (l.preusuel, l.nombre.toFloat))
-      .reduceByKey(_ + _)
-      .sortBy(_._2, ascending = false)
-      .take(10)
-      .foreach(println)
 
-    sc.stop
+
+
+    //----------------------------------------------------------------------
+    //Freestyle, faites ce que vous voulez ici !
+
+
+    // Stoppez le Spark context
   }
 }
--- dojo-spark/src/main/scala/fr/ippon/dojo/spark/Exercice2.scala
+++ dojo-spark/src/main/scala/fr/ippon/dojo/spark/Exercice2.scala
@@ -12,72 +12,48 @@ object Exercice2 {
   def main(args: Array[String]) {
 
     // instanciation du Spark Session
-    val spark = SparkSession
-      .builder
-      .master("local")
-      .appName("Coding Dojo - Exercice 2")
-      .getOrCreate
 
+    //----------------------------------------------------------------------
     // chargez le fichier dans un DataFrame ou DataSet et mettez le en cache
-    val firstNameDf = spark
-      .read
-      .option("header", true)
-      .option("delimiter", "\t")
-      .option("inferSchema", "true")
-      .csv("src/main/resources/data/insee/dpt2016.txt")
-      .cache
 
+
+    //----------------------------------------------------------------------
     // faites un comptage du nombre de ligne et affichez le dans le terminal
-    println("nombre de lignes du fichier : " + firstNameDf.count)
 
+
+    //----------------------------------------------------------------------
     // affichez le schéma du dataframe
-    println("\nSchéma des données :")
-    firstNameDf.printSchema
 
+
+    //----------------------------------------------------------------------
     // affichez les 20 premiers éléments
-    println("\nExtrait de données :")
-    firstNameDf.show
 
+
+    //----------------------------------------------------------------------
     // trouvez le nombre de personne qui ont le même prénom que vous
-    println("\nNombre de personnes avec un même prénom donné :")
-    firstNameDf
-      .where(col("preusuel") === "ANTOINE")
-      .groupBy("preusuel")
-      .sum("nombre")
-      .show
 
+
+    //----------------------------------------------------------------------
     // êtes-vous le seul à avoir eu votre prénom l'année de votre naissance dans votre département ?
-    println("\nNombre de personnes pour un nom, un département et une année donnés :")
-    firstNameDf
-      .where(
-        col("preusuel") === "ANTOINE"
-          && col("dpt") === "59"
-          && col("annais") === "1990")
-      .show
 
+
+    //----------------------------------------------------------------------
     // déterminez le nombre total de prénoms différents
-    println("\nNombre de prénoms différents : " + firstNameDf
-      .select("preusuel")
-      .distinct
-      .count)
 
+
+    //----------------------------------------------------------------------
     // trouvez le top 10 des prénoms utilisés pour l'année de votre naissance
     // vous pouvez caster le nombre en Float
-    val top10FirstNameDf = firstNameDf
-      .where(col("annais") === "1990")
-      .select(col("preusuel"), col("nombre"))
-      .groupBy("preusuel")
-      .agg(sum("nombre").as("sum"))
-      .orderBy(col("sum").desc)
-      .limit(10)
 
-    println("\nTop10 des prénoms pour une année donnée :")
-    top10FirstNameDf.show
 
+    //----------------------------------------------------------------------
     // affichez le plan d'exécution de ce dernier dataframe
-    println("\nPlan d'exécution du DataFrame :")
-    top10FirstNameDf.explain
 
-    spark.stop
+
+    //----------------------------------------------------------------------
+    //Freestyle, faites ce que vous voulez ici !
+
+
+    // Stoppez le Spark session
   }
 }
